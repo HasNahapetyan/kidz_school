f88778158c19c976c4a70bac8b138a028f7cffdf
Hasmik <1hasmiknahapetyan@gmail.com>
2023-06-03 15:28:52 +0400

    Initial commit

---
+++ src/main/java/com/example/kidz_school/KidzSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.kidz_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidzSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidzSchoolApplication.class, args);
+    }
+
+}
